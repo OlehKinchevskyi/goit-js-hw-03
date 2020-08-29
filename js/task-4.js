1802ad426ee260f0ef5f9eaa4cbae71841555809
Oleh Kinchevskyi <Oleh.Kinchevskyi@gmail.com>
2020-08-29 14:41:51 +0300

    Додає 3 та 4 завдання

---
+++ js/task-4.js
@@ -0,0 +1,28 @@
+//Суммирование значений свойств объекта
+
+const countTotalSalary = function (employees) {
+  'use strict';
+  // Write code under this line
+  let total = 0;
+  for (const employe of Object.values(employees)) {
+    total += employe;
+  }
+  return total;
+};
+
+// Объекты и ожидаемый результат
+const developers = {
+  mango: 300,
+  poly: 250,
+  alfred: 450,
+};
+//console.log(countTotalSalary(developers));
+// 1000
+
+const supports = {
+  kiwi: 200,
+  lux: 150,
+  chelsy: 150,
+};
+//console.log(countTotalSalary(supports));
+// 500
